a896e4272fa3fb0b51228881f194d5fa26a44b8c
Karthik <gkarthikchary2001@gmail.com>
2026-01-07 17:31:28 +0530

    convert home and heckout into asynch await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -11,10 +11,13 @@ function HomePage({ cart }) {
     const [products, setProducts] = useState([])
 
     useEffect(() => {
-        axios.get("/api/products")
-            .then((response) => {
-                setProducts(response.data)
-            })
+        const getHomeData = async () => {
+            const response = await axios.get("/api/products")
+            setProducts(response.data)
+        }
+
+        getHomeData()
+
     }, [])
 
     return (
